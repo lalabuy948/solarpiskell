17593eae4a84095720ba5955a090414b2aca057e
Daniil Popov <lalabuy9948@gmail.com>
2019-07-08 20:16:43 +0200

    [SAH-0] feat: Correct versions

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Web.Scotty
 
+routes :: ScottyM ()
+routes = do
+  get "/hello" $ do
+    text "hello world!"
+
 main = do
     putStrLn "Starting Server..."
-    scotty 3000 $ do
-        get "/hello" $ do
-            text "hello world!"
+    scotty 3000 routes
