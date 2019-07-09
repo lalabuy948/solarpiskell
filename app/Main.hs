139a9971a7016040a71ac13c3c9ee3e662b2ed85
Daniil Popov <lalabuy9948@gmail.com>
2019-07-09 15:48:44 +0200

    [SAH-0] feat: Setup basic solarObject rout

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,26 @@
 
 module Main where
 
+import GHC.Generics
 import Data.Monoid ((<>))
 import Data.Aeson (FromJSON, ToJSON)
 import GHC.Generics
 import Web.Scotty
+import Lib
+
+instance ToJSON SolarObject
+instance FromJSON SolarObject
+
 
 routes :: ScottyM ()
 routes = do
-  get "/hello" $ do
-    text "hello world!"
+  get "/allSolarObjects" $ do
+    json allSolarObjects
+
+  get "/solarObject/:id" $ do
+    id <- param "id"
+    json (filter (matchesId id) allSolarObjects)
 
 main = do
-    putStrLn "Starting Server..."
+    putStrLn "Starting Server on localhost:3000 ..."
     scotty 3000 routes
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,12 @@
-module Lib
-    ( printSun
-    ) where
+{-# LANGUAGE DeriveGeneric #-}
+
+module Lib where
 
 import Data.Monoid ((<>))
 import Data.Aeson (FromJSON, ToJSON)
 import GHC.Generics
 
-
-data SolarObject = SolarObject { objecttId     :: Int
+data SolarObject = SolarObject { objectId      :: Int
                                , objectName    :: String
                                , isPlanet      :: Bool
                                , aroundMoons   :: [SolarObject]
@@ -15,11 +14,46 @@ data SolarObject = SolarObject { objecttId     :: Int
                                , objectVolume  :: Int
                                , objectGravity :: Int
                                , aroundPlanets :: [SolarObject]
-                               } deriving (Eq, Show, Read)
+                               } deriving (Show, Read, Generic)
 
+matchesId :: Int -> SolarObject -> Bool
+matchesId id object = objectId object == id
+
+{-
+ - Sun
+ -}
 sun :: SolarObject
-sun = SolarObject 1 "Sun" False [] 15 21 10 []
+sun = SolarObject 0 "Sun" False [] 15 21 10 allSolarPlanets
 
-printSun :: IO ()
-printSun = do
-  print $ sun
+{-
+ - Main objects - Planets
+ -}
+mercury :: SolarObject
+mercury = SolarObject 1 "Mercury" True [] 15 21 10 []
+
+venus :: SolarObject
+venus = SolarObject 2 "Venus" True [] 15 21 10 []
+
+earth :: SolarObject
+earth = SolarObject 3 "Earth" True [] 15 21 10 []
+
+mars :: SolarObject
+mars = SolarObject 4 "Mars" True [] 15 21 10 []
+
+jupiter :: SolarObject
+jupiter = SolarObject 5 "Jupiter" True [] 15 21 10 []
+
+saturn :: SolarObject
+saturn = SolarObject 6 "Saturn" True [] 15 21 10 []
+
+uranus :: SolarObject
+uranus = SolarObject 7 "Uranus" True [] 15 21 10 []
+
+neptune :: SolarObject
+neptune = SolarObject 8 "Neptune" True [] 15 21 10 []
+
+allSolarPlanets :: [SolarObject]
+allSolarPlanets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
+
+allSolarObjects :: [SolarObject]
+allSolarObjects = [sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
