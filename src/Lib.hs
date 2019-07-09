2cae63c361ba69bede495392ed2a9ffc32de5a67
Daniil Popov <lalabuy9948@gmail.com>
2019-07-09 14:24:02 +0200

    [SAH-0] feat: Setup basic SolarObject sruct

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,25 @@
 module Lib
-    ( someFunc
+    ( printSun
     ) where
 
 import Data.Monoid ((<>))
 import Data.Aeson (FromJSON, ToJSON)
 import GHC.Generics
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+data SolarObject = SolarObject { objecttId     :: Int
+                               , objectName    :: String
+                               , isPlanet      :: Bool
+                               , aroundMoons   :: [SolarObject]
+                               , objectMass    :: Int
+                               , objectVolume  :: Int
+                               , objectGravity :: Int
+                               , aroundPlanets :: [SolarObject]
+                               } deriving (Eq, Show, Read)
+
+sun :: SolarObject
+sun = SolarObject 1 "Sun" False [] 15 21 10 []
+
+printSun :: IO ()
+printSun = do
+  print $ sun
